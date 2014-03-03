6255a4e7185bd1cd2c0c1f9bca860ab650dcd043
trikitrok <trikitrok@gmail.com>
2014-03-03 10:33:47 +0100

    Added factory method to environment and copied into it the code to get the college name from the environment variables. Not using it yet.

--- code/Environment.h
+++ code/Environment.h
@@ -5,10 +5,12 @@
 class Environment
 {
 public:
-  Environment(const std::string & college);
   virtual ~Environment();
 
+  static Environment * createEnvironment();
+
 private:
+  Environment(const std::string & college);
   std::string college;
 };
 
